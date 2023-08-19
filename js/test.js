ac2b4d5bef72bd24464748964200ec9d89f4a39b
farhan-71 <farhanmazumder75@gmail.com>
2023-08-19 07:15:49 +0600

    area calculation section interection is added.

--- js/test.js
+++ js/test.js
@@ -1,18 +1,4 @@
 // Utiliti functions
-/* 
-function getInputValue(fildId) {
-    const inputFild = document.getElementById(fildId);
-    const inputFildValue = inputFild.value;
-    inputFild.value = "";
-    if (!inputFildValue) {
-      alert("Invalid input!");
-      return 0;
-    }
-    //   const inputFildFixed = inputFildValue.toFixed(3);
-    const inputFildFloat = parseFloat(inputFildValue);
-    return inputFildFloat.toFixed(3);
-  }
- */
 function getInputValue(fildId) {
   const inputFild = document.getElementById(fildId);
   const inputFildValue = parseFloat(inputFild.value);
@@ -25,36 +11,30 @@ function getInputValue(fildId) {
 }
 
 function showArea(elementId, areaTotal) {
-  //   document.getElementById(elementId).innerText = areaText;
   const element = document.getElementById(elementId);
   element.classList.add("-mt-5", "mb-1");
   element.innerHTML = `
-  <p class="text-xl font-semibold">Area: <span id="areaTotal"></span> cm<sup>2</sup></p>
-  `;
-  //   const areaTotalFixed = areaTotal.toFixed(3);
-  // document.getElementById("areaTotal").firstChild.nodeValue = areaTotal;
-  document.getElementById("areaTotal").innerText = areaTotal;
+      <p class="text-xl font-semibold">Area: <span id="area-total"></span> cm<sup>2</sup></p>
+      `;
+  document.getElementById("area-total").innerText = areaTotal;
 }
 function areaEntry(areaType, area) {
   const entryId = document.getElementById("area-entry");
   const div = document.createElement("div");
   const count = entryId.childElementCount;
   div.innerHTML = `
-    <p class ="text-xl">${count}. ${areaType} ${area} cm<sup>2</sup></p>
-    <button class="btn btn-sm btn-primary text-white text-xs"><p>convart to m<sup>2</sup></p>
-</button>
-  `;
+        <p class ="text-xl">${count}. ${areaType} ${area} cm<sup>2</sup></p>
+        <button class="btn btn-sm btn-primary text-white text-xs"><p>convart to m<sup>2</sup></p>
+    </button>
+      `;
   div.classList.add("flex", "items-center", "gap-3", "justify-center");
   entryId.appendChild(div);
 }
-// <div class="flex items-center gap-3"></div>
 
 //Triangle part
 document.getElementById("btn-triangle").addEventListener(`click`, function () {
   const base = getInputValue("base-fild");
   const height = getInputValue("height-fild");
-  //   document.getElementById("base-fild").value = "";
-  //   document.getElementById("height-fild").value = "";
   document.getElementById("triangle-area").style.display = "none";
 
   if (base === 0 || height === 0) {
@@ -62,9 +42,6 @@ document.getElementById("btn-triangle").addEventListener(`click`, function () {
   }
   document.getElementById("triangle-area").style.display = "block";
   const area = 0.5 * base * height;
-
-  //   console.log(area);
-  // const areaTotal = area;
   showArea("triangle-area", area);
   areaEntry("Triangle", area);
 });
@@ -79,7 +56,6 @@ document.getElementById("btn-rectangle").addEventListener(`click`, function () {
   }
   document.getElementById("rectangle-area").style.display = "block";
   const area = width * length;
-
   showArea("rectangle-area", area);
   areaEntry("Rectangle", area);
 });
@@ -90,8 +66,6 @@ document
   .addEventListener("click", function () {
     const base = getInputValue("pbase-fild");
     const height = getInputValue("pheight-fild");
-    // console.log(base);
-    // console.log(height);
     document.getElementById("parallelogram-area").style.display = "none";
 
     if (base === 0 || height === 0) {
@@ -99,8 +73,6 @@ document
     }
     document.getElementById("parallelogram-area").style.display = "block";
     const area = base * height;
-    // console.log(area);
-
     showArea("parallelogram-area", area);
     areaEntry("Parallelogram", area);
   });
@@ -109,9 +81,7 @@ document
 document.getElementById("btn-rhombus").addEventListener(`click`, function () {
   const d1 = getInputValue("d1-fild");
   const d2 = getInputValue("d2-fild");
-
   document.getElementById("rhombus-area").style.display = "none";
-
   if (d1 === 0 || d2 === 0) {
     return;
   }
@@ -125,9 +95,7 @@ document.getElementById("btn-pentagon").addEventListener(`click`, function () {
   const length = getInputValue("pentagon-fild");
   const pi = Math.PI;
   const tan = Math.tan(pi / 5);
-
   document.getElementById("pentagon-area").style.display = "none";
-
   if (length === 0) {
     return;
   }
@@ -142,9 +110,7 @@ document.getElementById("btn-ellipse").addEventListener(`click`, function () {
   const a = getInputValue("ellipse-a");
   const b = getInputValue("ellipse-b");
   const pi = Math.PI;
-
   document.getElementById("ellipse-area").style.display = "none";
-
   if (a === 0 || b === 0) {
     return;
   }
